fd86386eb93b8c15d8eff7a427c98c0fecfd9b5d
gchandra10 <gchandra10>
2022-02-26 04:29:02 +0000

    ISO8601 - Spark SQL Sample

--- nb01.py
+++ nb01.py
@@ -2,3 +2,32 @@
 # MAGIC %md
 # MAGIC 
 # MAGIC ### Connecting to Github Repo from Databricks
+
+# COMMAND ----------
+
+data = [('2021-01-25T13:33:44.343Z'),('2021-02-25T11:12:11.103Z')]
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+df = spark.createDataFrame(data,StringType())
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+df.createOrReplaceTempView("vw_sample")
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select date_format(value,'yyyy-MM-dd HH:mm:ss') from vw_sample
+
+# COMMAND ----------
+
+
