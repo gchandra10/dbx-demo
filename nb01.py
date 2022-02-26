2dfb9bb8c11503bd37ecf7bee54b6adabc469f5c
gchandra10 <gchandra10>
2022-02-26 04:34:02 +0000

    update branch

--- nb01.py
+++ nb01.py
@@ -10,12 +10,18 @@ data = [('2021-01-25T13:33:44.343Z'),('2021-02-25T11:12:11.103Z')]
 # COMMAND ----------
 
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
 
 df = spark.createDataFrame(data,StringType())
 
 # COMMAND ----------
 
 display(df)
+
+# COMMAND ----------
+
+df1 = df.withColumn("dt",to_timestamp('value',"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+display(df1)
 
 # COMMAND ----------
 
